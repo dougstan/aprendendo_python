1bde444b4f2446f28ef9d2f356a0ac82e72c526f
Douglas Stanichesch <60761186+dougstan@users.noreply.github.com>
2020-10-06 21:07:58 -0300

    Descrição do exercício Média Harmônica

--- media_harmonica.py
+++ media_harmonica.py
@@ -1,1 +1,4 @@
-# Média harmônica amortizada
+"""
+A média harmônica amortizada é definida como: 𝐻=𝑁/(∑1𝑛𝑖+𝑋𝑁𝑖=1−𝑋) onde N é o número de notas a serem usadas na média, ni é a i-ésimanota, e X é o fator de amortização. Implemente uma função em Python que calcule a média de 3 notas de um aluno digitadas no teclado, com fator de amortização igual a 4. Em seguida, informe se o aluno passou (média >= 5) ou não (média < 5)
+"""
+
